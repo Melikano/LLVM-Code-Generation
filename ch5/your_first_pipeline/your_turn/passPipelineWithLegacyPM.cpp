ba7ba95266b9e241d5ee1fe3f889f111f213a7cb
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2025-10-03 20:02:03 -0400

    add pass pipeline with legacy and new pass managers

--- ch5/your_first_pipeline/your_turn/passPipelineWithLegacyPM.cpp
+++ ch5/your_first_pipeline/your_turn/passPipelineWithLegacyPM.cpp
@@ -1,5 +1,22 @@
 #include "llvm/IR/LegacyPassManager.h" // For legacy::PassManager.
-
+#include "llvm/Transforms/Utils.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/IPO/AlwaysInliner.h"
 using namespace llvm;
 
-void runYourTurnPassPipelineForLegacyPM(Module &MyModule) {}
+void runYourTurnPassPipelineForLegacyPM(Module &MyModule) {
+
+    legacy::PassManager LPM;
+
+    FunctionPass *mem2regPass = createPromoteMemoryToRegisterPass();
+    LPM.add(mem2regPass);
+
+    FunctionPass *instCombinePass = createInstructionCombiningPass();
+    LPM.add(instCombinePass);
+
+    Pass *alwaysInline = createAlwaysInlinerLegacyPass();
+    LPM.add(alwaysInline);
+
+    LPM.run(MyModule);
+    
+}
