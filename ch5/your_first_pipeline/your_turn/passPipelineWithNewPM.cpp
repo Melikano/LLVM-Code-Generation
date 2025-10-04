9de2f9b28499ae4878f5566e834772a91f21b5e3
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2025-10-03 20:10:53 -0400

    hook-up PassInstrumentationAnalysis and logging capabilities

--- ch5/your_first_pipeline/your_turn/passPipelineWithNewPM.cpp
+++ ch5/your_first_pipeline/your_turn/passPipelineWithNewPM.cpp
@@ -2,9 +2,9 @@
 #include "llvm/IR/PassManager.h" // For the new PassManager.
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/StandardInstrumentations.h"
-#include "llvm/Transforms/IPO/AlwaysInliner.h"       
+#include "llvm/Transforms/IPO/AlwaysInliner.h"
 #include "llvm/Transforms/InstCombine/InstCombine.h"
-#include "llvm/Transforms/Utils/Mem2Reg.h"           
+#include "llvm/Transforms/Utils/Mem2Reg.h"
 
 using namespace llvm;
 
@@ -16,9 +16,26 @@ void runYourTurnPassPipelineForNewPM(Module &MyModule)
     FunctionAnalysisManager FAM;
     ModuleAnalysisManager MAM;
 
+    PassInstrumentationCallbacks PIC;
+    PrintPassOptions PrintPassOpts;
+    PrintPassOpts.Verbose = true;
+    PrintPassOpts.SkipAnalyses = false;
+    PrintPassOpts.Indent = true;
+    StandardInstrumentations SI(CTX, true, false, PrintPassOpts);
+    SI.registerCallbacks(PIC, &MAM);
+
+    MAM.registerPass([&]
+                     { return PassInstrumentationAnalysis(&PIC); });
+    FAM.registerPass([&]
+                     { return PassInstrumentationAnalysis(&PIC); });
+
+    MAM.registerPass([&]
+                     { return FunctionAnalysisManagerModuleProxy(FAM); });
+    FAM.registerPass([&]
+                     { return ModuleAnalysisManagerFunctionProxy(MAM); });
+
     ModulePassManager ModPM;
     FunctionPassManager FunPM;
-
 
     PassBuilder PB;
     PB.registerFunctionAnalyses(FAM);
