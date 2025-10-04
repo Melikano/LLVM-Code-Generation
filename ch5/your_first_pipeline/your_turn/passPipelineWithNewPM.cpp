dbafba82c7e5a9f44b319dbf062e178ca7b7d68a
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2025-10-03 20:02:03 -0400

    add pass pipeline with legacy and new pass managers

--- ch5/your_first_pipeline/your_turn/passPipelineWithNewPM.cpp
+++ ch5/your_first_pipeline/your_turn/passPipelineWithNewPM.cpp
@@ -1,6 +1,33 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/PassManager.h" // For the new PassManager.
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/StandardInstrumentations.h"
+#include "llvm/Transforms/IPO/AlwaysInliner.h"       
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/Utils/Mem2Reg.h"           
 
 using namespace llvm;
 
-void runYourTurnPassPipelineForNewPM(Module &MyModule) {}
+void runYourTurnPassPipelineForNewPM(Module &MyModule)
+{
+
+    LLVMContext &CTX = MyModule.getContext();
+
+    FunctionAnalysisManager FAM;
+    ModuleAnalysisManager MAM;
+
+    ModulePassManager ModPM;
+    FunctionPassManager FunPM;
+
+
+    PassBuilder PB;
+    PB.registerFunctionAnalyses(FAM);
+    PB.registerModuleAnalyses(MAM);
+
+    FunPM.addPass(PromotePass());
+    FunPM.addPass(InstCombinePass());
+    ModPM.addPass(createModuleToFunctionPassAdaptor(std::move(FunPM)));
+
+    ModPM.addPass(AlwaysInlinerPass());
+    ModPM.run(MyModule, MAM);
+}
